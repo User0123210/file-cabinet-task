4c31389a973099a7045556b7cf1a45bee7cfb6be
User012345 <voprosichek@gmail.com>
2025-02-02 02:33:51 +0300

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/FileCabinetApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileCabinetApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Creates and runs Console App.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
