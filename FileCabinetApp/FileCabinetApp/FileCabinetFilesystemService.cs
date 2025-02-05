dfd6f8eb2ee51df1bb907c1df083b45db2024604
User0123210 <voprosichek@gmail.com>
2025-02-05 05:42:22 +0300

    Implement FindByLastName for FileCabinetFilesystemService

--- FileCabinetApp/FileCabinetApp/FileCabinetFilesystemService.cs
+++ FileCabinetApp/FileCabinetApp/FileCabinetFilesystemService.cs
@@ -274,7 +274,49 @@ namespace FileCabinetApp
         /// </summary>
         /// <param name="lastName">Last name of the records to seek.</param>
         /// <returns>Array of the found records with the specified lastName.</returns>
-        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName) => throw new NotImplementedException();
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            List<FileCabinetRecord> records = new ();
+            byte[] buffer = new byte[RecordSize];
+            this.stream.Position = 0;
+            int id;
+            string firstName;
+            string recordLastName;
+            int year;
+            int month;
+            int day;
+            short status;
+            decimal salary;
+            char permissions;
+            int[] copyDecimal = new int[4];
+
+            while (this.stream.Read(buffer, 0, RecordSize) != 0)
+            {
+                recordLastName = Encoding.UTF8.GetString(buffer[126..246]).TrimEnd('\0');
+
+                if (recordLastName == lastName)
+                {
+                    id = BitConverter.ToInt32(buffer, 2);
+                    firstName = Encoding.UTF8.GetString(buffer[6..126]).TrimEnd('\0');
+                    year = BitConverter.ToInt32(buffer, 246);
+                    month = BitConverter.ToInt32(buffer, 250);
+                    day = BitConverter.ToInt32(buffer, 254);
+                    DateTime dateOfBirth = new (year, month, day);
+                    status = BitConverter.ToInt16(buffer, 258);
+                    copyDecimal[0] = BitConverter.ToInt32(buffer, 274);
+                    copyDecimal[1] = BitConverter.ToInt32(buffer, 278);
+                    copyDecimal[2] = BitConverter.ToInt32(buffer, 282);
+                    copyDecimal[3] = BitConverter.ToInt32(buffer, 286);
+                    salary = new decimal(copyDecimal);
+                    permissions = BitConverter.ToChar(buffer, 290);
+                    records.Add(new FileCabinetRecord() { Id = id, FirstName = firstName, LastName = lastName, DateOfBirth = dateOfBirth, Status = status, Salary = salary, Permissions = permissions });
+                }
+
+                this.stream.Position += RecordSize;
+            }
+
+            return records.AsReadOnly();
+        }
 
         /// <summary>
         /// Looks for records with dateOfBirth property equal to the specified date parameter.
