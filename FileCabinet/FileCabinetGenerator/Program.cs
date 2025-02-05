24cb9cf84ec6772cbfc2bbc0a563367ece8e05b2
User0123210 <voprosichek@gmail.com>
2025-02-05 10:03:17 +0300

    Add implementation of output-type, output, records-amount, start-id

--- FileCabinet/FileCabinetGenerator/Program.cs
+++ FileCabinet/FileCabinetGenerator/Program.cs
@@ -1,15 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#pragma warning disable CA1031, CA1801, IDE0060
 namespace FileCabinetGenerator
 {
+    /// <summary>
+    /// Represents FileCabinetGenerator Console App.
+    /// </summary>
     internal class Program
     {
-        static void Main(string[] args)
+        private const string DeveloperName = "Serafima Mochalova";
+        private static string outputType = "csv";
+        private static int recordsAmount;
+        private static int startId = 1;
+        private static bool isRunning = true;
+
+        /// <summary>
+        /// Runs Console Application.
+        /// </summary>
+        /// <param name="args">The arguments of the application.</param>
+        public static void Main(string[] args)
         {
+            Console.WriteLine($"File Cabinet Generator, developed by {Program.DeveloperName}");
+            Console.WriteLine();
+            FileStream? outputFile = null;
+
+            do
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                line = line?.Trim();
+                var inputs = line != null ? line.Split(new char[] { '=', ' ' }, 3) : new string[] { string.Empty, string.Empty };
+                const int commandIndex = 0;
+                string command = inputs[commandIndex];
+                var parseInputs = inputs;
+
+                if (command == "exit")
+                {
+                    isRunning = false;
+                }
+
+                while (parseInputs.Length > 1)
+                {
+                    command = parseInputs[commandIndex];
+                    var paramValue = parseInputs[commandIndex + 1];
+
+                    if (command == "--output-type" || command == "-t")
+                    {
+                        switch (paramValue.ToUpperInvariant())
+                        {
+                            case "CSV":
+                                outputType = "csv";
+                                break;
+                            case "XML":
+                                outputType = "xml";
+                                break;
+                        }
+
+                        Console.WriteLine($"Using {outputType} output type.");
+                    }
+                    else if (command == "--output" || command == "-o")
+                    {
+                        try
+                        {
+                            outputFile = new (paramValue, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                            Console.WriteLine($"Using {paramValue} outputFile.");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("An error ocured during the specifying of output file.");
+                        }
+                    }
+                    else if (command == "--records-amount" || command == "-a")
+                    {
+                        if (int.TryParse(paramValue, out int amount))
+                        {
+                            if (amount >= 0)
+                            {
+                                recordsAmount = amount;
+                                Console.WriteLine($"Using {recordsAmount} records amount.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Records amount should be more than 0.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Records amount should be an integer.");
+                        }
+                    }
+                    else if (command == "--start-id" || command == "-i")
+                    {
+                        if (int.TryParse(paramValue, out int id))
+                        {
+                            startId = id;
+                            Console.WriteLine($"Start id is {startId}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Start id should be an integer.");
+                        }
+                    }
+
+                    if (parseInputs.Length > 2)
+                    {
+                        parseInputs = parseInputs[commandIndex + 2].Split(new char[] { ' ', '=' }, 3);
+                    }
+                    else if (parseInputs.Length <= 2)
+                    {
+                        Console.WriteLine($"{recordsAmount} records were written to {outputFile}");
+                        parseInputs = Array.Empty<string>();
+                    }
+                }
+            }
+            while (isRunning);
+
+            outputFile?.Close();
         }
     }
 }
